834cd20145716bd7bb9c373995afd574846831e7
Salih Okur <salih.okur@tubitak.gov.tr>
2023-06-10 01:49:27 +0300

    first

---
+++ build/CMakeFiles/akisp11.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/akis/Downloads/D-shared-library/src/Hello.cpp" "CMakeFiles/akisp11.dir/src/Hello.cpp.o" "gcc" "CMakeFiles/akisp11.dir/src/Hello.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hello_binary.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/hello_binary.dir/src/main.cpp.o: \
+ /home/akis/Downloads/D-shared-library/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/akis/Downloads/D-shared-library/include/shared/Hello.h
