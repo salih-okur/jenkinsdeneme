a2527315b2b1cc8a84f5ab5e438c9ebd18a0701e
Salih Okur <salih.okur@tubitak.gov.tr>
2023-06-10 02:15:37 +0300

    asdasd

--- build/CMakeFiles/hello_binary.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/hello_binary.dir/src/main.cpp.o: \
- /home/akis/Downloads/D-shared-library/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/akis/Downloads/D-shared-library/include/shared/Hello.h
